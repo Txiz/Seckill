6e8a3b9524e9e4d9847dd9546a0c8e21707fc33a
Txiz <853954828@qq.com>
2021-06-22 18:17:38 +0800

    初始化

--- src/main/java/com/txiz/seckill/SeckillApplication.java
+++ src/main/java/com/txiz/seckill/SeckillApplication.java
@@ -3,6 +3,9 @@ package com.txiz.seckill;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Txiz
+ */
 @SpringBootApplication
 public class SeckillApplication {
 
